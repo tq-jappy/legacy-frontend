5bb3f00c2866dbfe7b89a67f18978024960135e4
Takumi Endo <tq-ed@v7.com>
2019-09-05 22:47:57 +0900

    separete main file

--- js/script.js
+++ js/script.js
@@ -1,35 +1,21 @@
 import $ from 'jquery';
+import { readData } from './reader';
+import {
+  writeNextTodo,
+  writeTodoCount,
+  toggleTodoList,
+  toggleTodoEmpty,
+  addTodo,
+  removeTodo
+} from './writer';
 
 const updateAll = () => {
-  const count = $('.todo').length;
-  const next = $('.todo input').first();
-  const nextTodoText = count ? next.val() : '未登録';
+  const { count, nextTodoText } = readData();
 
-  $('#nextTodo').text(`次のTODO: ${nextTodoText}`);
-  $('#todoCount').text(`(全${count}件)`);
-
-  if (count) {
-    $('#todoList').show();
-    $('#todoEmpty').hide();
-  } else {
-    $('#todoList').hide();
-    $('#todoEmpty').show();
-  }
-};
-
-const addTodo = () => {
-  const wrapper = $('<div>');
-  wrapper.addClass('todo');
-
-  const input = $('<input>');
-  input.attr('type', 'text');
-
-  const deleteButton = $('<button>');
-  deleteButton.addClass('delete').text('削除');
-
-  wrapper.append(input);
-  wrapper.append(deleteButton);
-  $('#todoList').append(wrapper);
+  writeNextTodo(nextTodoText);
+  writeTodoCount(count);
+  toggleTodoList(count);
+  toggleTodoEmpty(count);
 };
 
 $(function() {
@@ -43,9 +29,7 @@ $(function() {
   });
 
   $('#todoList').on('click', '.delete', function() {
-    $(this)
-      .closest('.todo')
-      .remove();
+    removeTodo(this);
     updateAll();
   });
 
