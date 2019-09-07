f366e5f31b50ac898357b2b6748c43bbc341259e
Takumi Endo <tq-ed@v7.com>
2019-09-07 19:44:45 +0900

    add root component

---
+++ src/TodoApp.vue
@@ -0,0 +1,11 @@
+<template>
+  <div v-if="false"></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
--- src/mount.js
+++ src/mount.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
+import TodoApp from './TodoApp';
 import NextTodo from './NextTodo';
 import TodoCount from './TodoCount';
 import TodoEmpty from './TodoEmpty';
 import Sample from './Sample';
 
+new Vue(TodoApp).$mount('#app');
 new Vue(NextTodo).$mount('#nextTodo');
 new Vue(TodoCount).$mount('#todoCount');
 new Vue(TodoEmpty).$mount('#todoEmpty');
